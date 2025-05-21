7cd5fa790fea33273de95160f4a718528e7ce490
Elaine Ferreira <elaineferreira.dev@gmail.com>
2025-05-20 22:38:45 -0300

    feat: add configs for django project

---
+++ config/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from webhooks.views import WebhooksOrderView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/v1/webhooks/order/", WebhooksOrderView.as_view(), name="webhook-order"),
+]
